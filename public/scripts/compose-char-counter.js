7ce984c457980fa3bee2ebb7ac5b01e04883fcbc
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2019-11-08 08:40:27 -0800

    implement character counter in form

---
+++ public/scripts/compose-char-counter.js
@@ -0,0 +1,22 @@
+/* eslint-disable indent */
+/* eslint-disable no-undef */
+$(document).ready(function () {
+  //get input text
+  const $input = $('textarea');
+  //get counter element
+  const $counter = $('.counter');
+
+  //event listener for changing counter for input characters
+  $input.on('input', function () {
+    const count = 140 - $input.val().length;
+
+    //if input charachters exceed more than 140
+    if (count <= 0) {
+      $counter.addClass('counter_error');
+    } else {
+      $counter.removeClass('counter_error');
+    }
+    $counter.text(count);
+  });
+
+});
