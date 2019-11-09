6d88fb820a20b197cc451b53b41584a0a4a7f204
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2019-11-09 09:11:03 -0800

    fix bug for button toggler which was placed out of form submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,9 +1,6 @@
 /* eslint-disable indent */
 /* eslint-disable no-undef */
-
 //escape from XSS(Cross-Site Scripting)
-// const moment = require('moment');
-
 const escape = function (str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
@@ -12,13 +9,8 @@ const escape = function (str) {
 
 //create tweet using user object
 const createTweetElement = function (object) {
-
   //safe text to use for content
   const safeHTML = `<p class="tweet-content">${escape(object.content.text)}</p>`;
-
-  // let time = moment(object.created_at).startOf('minutes').fromNow();
-
-  // console.log(time);
   //substitute markup content with data in given object
   const markup = `
   <article class='tweet'>
@@ -35,17 +27,14 @@ const createTweetElement = function (object) {
       <i class="fas fa-flag icon"></i>
   </footer>
 </article>`;
-
   //return markup
   return markup;
 };
 
 //render tweets using array of objects
 const renderTweets = function (array) {
-
   // loops through tweets
   for (const item of array) {
-
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
     $('.tweets-container').append(createTweetElement(item));
@@ -68,41 +57,29 @@ const loadTweets = function () {
 };
 
 $(document).ready(function () {
-
   //render tweets before submission
   loadTweets();
-
-  //hide error on default
-  $error.slideUp();
-
   //add onclick event handler
   $('#submit-tweet').submit(function (event) {
-
     //prevent page from refreshing for fetching data
     event.preventDefault();
-
     //text in input filed
     const input = $('#input-field').val();
-
     //set error message not shown in default
     const $errorContent = $('#error-content');
     const $error = $('.error');
+    //hide error on default
     $error.slideUp();
-
     //check if text length doesn't exceed more than 140 characters
     if (input.length >= 140) {
-
       //show error message
       $error.slideDown();
       $errorContent.text(`Too Long! It shouldn't exceed more than 140 characters!!!`);
-
       //check if user has typed anything
     } else if (input.length === 0) {
-
       //show error message
       $error.slideDown();
       $errorContent.text(`You didn't type anything yet!!!`);
-
     } else {
       //fetching tweets using ajax post
       $.ajax({
@@ -115,11 +92,11 @@ $(document).ready(function () {
           loadTweets();
         })
       });
+      //clear text field and add focus
+      $('#submit-tweet').trigger("reset");
+      $('.counter').text("140");
+      $('textarea').focus();
     }
-    //clear text field and add focus
-    $('#submit-tweet').trigger("reset");
-    $('.counter').text("140");
-    $('textarea').focus();
   });
 }
 
